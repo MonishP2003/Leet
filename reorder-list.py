f84953377a4759b2957692ca2a800d4d5af9541d
P Monish <129496247+MonishP2003@users.noreply.github.com>
2024-03-26 10:56:27 +0530

    Add files via upload

---
+++ reorder-list.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        curr = head
+        n1 = head
+        n2 = head
+        result = []
+        while n2:
+            result.append(n2.val)
+            n2 = n2.next
+        
+        left, right = 0, len(result)-1
+        while left <= right:
+            n1.val = result[left]
+            n1 = n1.next
+            if n1:
+                n1.val = result[right]
+                n1 = n1.next
+            left += 1
+            right -= 1
